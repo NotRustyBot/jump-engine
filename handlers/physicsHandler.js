b0db621310ceb9c91e5b3b269375cae1096e001b
Andrej Karovin <andrej.karovin2000@gmail.com>
2021-10-03 05:55:35 +0200

    Area progress

--- handlers/physicsHandler.js
+++ handlers/physicsHandler.js
@@ -2,8 +2,8 @@ const { BaseObject } = require("./objectHandler");
 const { Vector } = require("./types");
 const { UpdateHandler } = require("./updateHandler");
 
-class PhysicsObject extends BaseObject{
-    constructor(){
+class PhysicsObject extends BaseObject {
+    constructor() {
         super();
         this.position = new Vector();
     }
@@ -11,7 +11,45 @@ class PhysicsObject extends BaseObject{
 
 exports.PhysicsObject = PhysicsObject;
 
-class Area{
-    constructor(){
+class Area {
+    /**
+     * @param {Vector} position
+     */
+    constructor(position) {
+        /**@type {Set<PhysicsObject>} */
+        this.members = new Set();
+        this.gridPosition = new Vector(position.x, position.y);
+        this.positionIndex = Area.toGridIndex(this.gridPosition);
+
+        Area.areas.set(this.positionIndex, this);
+    }
+
+    /**
+     * @param {Vector} vector
+     * @returns {number}
+     */
+    static toGridIndex(vector) {
+        return ((vector.x & 0xFFFF) << 16) | (vector.y & 0xFFFF);
+    }
+
+    static areaSize = 1000;
+    /**@type {Map<number,Area>} */
+    static areas = new Map();
+
+    /**
+     * @param {PhysicsObject} physicsObject
+     */
+    static addObject(physicsObject) {
+        let gridPosition = Area.toGrid(physicsObject.position);
+        let area = Area.areas.get(Area.toGridIndex(gridPosition)) || new Area(gridPosition);
+        area.members.add(physicsObject);
+    }
+
+    /**
+     * @param {Vector} position
+     * @returns {Vector} position in Area grid
+     */
+    static toGrid(position) {
+        return new Vector(Math.floor(position.x / Area.areaSize), Math.floor(position.y / Area.areaSize));
     }
 }
