ccc31c6deb9e4f86eed9fbff21dfd33e262a4004
Andrej Karovin <andrej.karovin2000@gmail.com>
2022-03-14 21:34:51 +0100

    add makeid

--- src/helpers.ts
+++ src/helpers.ts
@@ -10,6 +10,16 @@ export function toDeg(x: number): number {
     return (x * 180) / Math.PI;
 }
 
-export function transfer(to: any, from: any){
-    for(var k in from) to[k]=from[k];
+export function transfer(to: any, from: any) {
+    for (var k in from) to[k] = from[k];
+}
+
+export function makeId(length: number) {
+    var result = "";
+    var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    var charactersLength = characters.length;
+    for (var i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
 }
